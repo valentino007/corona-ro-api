8e5fc95ef835e8e6f0c90da350ade5705bbf2987
valipank <valentin.pancescu@gmx.com>
2022-02-20 09:20:15 +0200

    Delete api directory

--- api/v1/judete/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json; charset=UTF-8');
-header("Access-Control-Allow-Methods: GET");
-// just prints the json file
-echo file_get_contents("../../json/judete.json");
-?>
